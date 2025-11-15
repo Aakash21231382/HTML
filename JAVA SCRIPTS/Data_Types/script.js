7c650a63de40252da04d19719c19a84fbff30c35
Aakash21231382 <Aakashkumar0728@gmail.com>
2025-11-15 19:25:26 +0530

    data types in java script

---
+++ JAVA SCRIPTS/Data_Types/script.js
@@ -0,0 +1,40 @@
+//let a = "aakash";
+//a = '123';
+//a=true;
+ //console.log('The value of a is ', a);
+//The typeof is to check this type of value
+
+ //console.log('The type of a  is', typeof(a))
+
+//  let a = 123;
+
+//  a ='demo';
+//  a=false;
+//  let b;
+//  let sum = null;
+//  console.log('the vake of sum is ', sum)
+//  console.log('the vake of sum is ', typeof(sum))
+
+
+// let v1 = 12;
+// console.log(v1+1);
+
+// let v2="12";
+// console.log(v2+1);
+// let abc = 123;
+//  let result=String(abc)
+// console.log('the value of abc is ', abc);
+// console.log('the type of abc is', typeof(abc))
+// console.log('the type of abc is', typeof(result))
+
+
+// let name = "aakash";
+// name=Number(name)
+// console.log('the value of name is ', name)
+// console.log('the value of name is ', typeof(name))
+
+let name = 1334
+name=Boolean(name)
+console.log('the value of name is ', name)
+console.log('the value of name is', typeof(name))
+
